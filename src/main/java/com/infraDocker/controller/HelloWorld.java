e3f4f14fe94cbc3274e64ea07577fa0159b60220
GustBitencourt <gubiten@gmail.com>
2021-11-18 17:00:05 -0300

    primeiro commit

---
+++ src/main/java/com/infraDocker/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.infraDocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String helloMessage() {
+        return "Vai Corinthians!!";
+    }
+
+}
---
+++ src/test/java/com/infraDocker/JesnkinsSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infraDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesnkinsSpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
